948b4aabb3dc21bff9243bbf242cea2242c4b7d7
mirshekari <37343306+mirshekari@users.noreply.github.com>
2021-01-02 18:35:00 +0330

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <SEO title="404: خطا آدرس اشتباه" />
+    <h1>404: خطا آدرس اشتباه</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
